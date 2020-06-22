3701caeda9e4ebdea98a47d6a884465e93681af6
Dusty McCord <Dustymccord@gmail.com>
2020-06-22 10:40:23 -0700

    add key player stats components to project.

--- src/js/App.js
+++ src/js/App.js
@@ -1,12 +1,14 @@
 import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Test from './Test';
+import KeyPlayersStats from './keyPlayStats/KeyPlayersStats';
 function App() {
   return (
     <Fragment>
       <Router>
         <Switch>
           <Route path='/test' component={Test} />
+          <Route path='/key-players-stats' component={KeyPlayersStats} />
           <Route exact path='/'>
             <h1>Hello from App</h1>
           </Route>
