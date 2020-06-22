980b0e438d353e7ab3b061c6d3ca3aa72e51383d
Dusty McCord <Dustymccord@gmail.com>
2020-06-22 11:07:45 -0700

    add news feed components to project.

--- src/js/App.js
+++ src/js/App.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Test from './Test';
 import KeyPlayersStats from './keyPlayStats/KeyPlayersStats';
+import NewsFeedControl from './newsFeed/NewsFeedControl';
 function App() {
   return (
     <Fragment>
@@ -9,6 +10,7 @@ function App() {
         <Switch>
           <Route path='/test' component={Test} />
           <Route path='/key-players-stats' component={KeyPlayersStats} />
+          <Route path='/news-feed' component={NewsFeedControl} />
           <Route exact path='/'>
             <h1>Hello from App</h1>
           </Route>
---
+++ src/js/newsFeed/NewsFeedControl.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import testJsonObject from './test_json_object.js';
+import '../../scss/_NewsFeedControl.scss';
+
+//Components
+
+import NewsFeedMain from './NewsFeedMain';
+import NewsFeedSideBar from './NewsFeedSideBar';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      masterNewsList: testJsonObject,
+      newsFeedMainData: testJsonObject[0],
+      newsFeedbackSideBarData: [testJsonObject[1], testJsonObject[2]],
+    };
+  }
+
+  //insert div to put newsFeedback sidebar components inside so they stack correctly
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='Container'>
+          <NewsFeedMain newsFeedMainData={this.state.newsFeedMainData} />
+          <div className='newsFeedSidebarContainer'>
+            <NewsFeedSideBar
+              newsFeedbackSideBarData={this.state.newsFeedbackSideBarData[0]}
+            />
+            <NewsFeedSideBar
+              newsFeedbackSideBarData={this.state.newsFeedbackSideBarData[1]}
+            />
+            <NewsFeedSideBar
+              newsFeedbackSideBarData={this.state.newsFeedbackSideBarData[1]}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
