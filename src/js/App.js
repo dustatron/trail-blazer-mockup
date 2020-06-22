cbbbe077ed431e2c09fdeddd29b881d599fa783b
Alex Skreen <skreenalex@gmail.com>
2020-06-22 15:11:14 -0700

    migrated components from previoius interns

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,14 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Test from './Test';
 import SimpleLinks from './simpleLinks';
 import KeyPlayersStats from './keyPlayStats/KeyPlayersStats';
-import NewsFeedControl from './newsFeed/NewsFeedControl';
+import Ad from './gameScore/ad';
+import LiveGame from './gameScore/LiveGame';
+import Nav from './gameScore/Nav';
+import PostGame from './gameScore/PostGame';
+import PreGame from './gameScore/PreGame';
+
+
+
 function App() {
   return (
     <Fragment>
@@ -12,6 +19,12 @@ function App() {
           <Route path='/test' component={Test} />
           <Route path='/key-players-stats' component={KeyPlayersStats} />
           <Route path='/news-feed' component={NewsFeedControl} />
+          <Route path='/ad' component={Ad} />
+          <Route path='/LiveGame' component={LiveGame} />
+          <Route path='/Nav' component={Nav} />
+          <Route path='/PostGame' component={PostGame} />
+          <Route path='/PreGame' component={PreGame} />
+
           <Route exact path='/'>
             <SimpleLinks />
           </Route>
