8eed008a89ce598aab66c3063305ba54dcb717b4
Dusty McCord <Dustymccord@gmail.com>
2020-06-22 16:31:18 -0700

    update keyPlayer with a temp hooks for state.

--- src/js/PreGame/KeyPlayers/index.js
+++ src/js/PreGame/KeyPlayers/index.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 ///////  KEY PLAYER INDEX //////
 const index = () => {
+  const [playerData, setPlayerData] = useState([]);
+  useEffect(() => {
+    //Get from API
+    setPlayerData([]);
+  }, []);
   return (
     <div>
       <h1>Key Players</h1>
