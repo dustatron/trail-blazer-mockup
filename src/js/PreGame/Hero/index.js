ebf57dc447457441d57bf32af26fde26e16e6322
Alex Skreen <skreenalex@gmail.com>
2020-06-25 14:27:46 -0700

    add structure for hero before adding styling

--- src/js/PreGame/Hero/index.js
+++ src/js/PreGame/Hero/index.js
@@ -1,12 +1,34 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import HeroImage from './HeroImage';
 
-///////  HERO INDEX //////
-const Index = () => {
-  return (
-    <div>
-      <h1>Hero Image</h1>
-    </div>
-  );
-};
-
-export default Index;
+ReactDOM.render(
+  <HeroImage
+    imageSrc="https://codetheweb.blog/assets/img/posts/full-image-hero/image.jpg"
+    color="#555555"
+    gradientDirection="to bottom right"
+    height="100vh"
+    opacity="0.8"
+    childrenStyles={{ color: "#f7f7f7" }}
+    parallax
+    textPosition="center"
+  >
+    <h1>Blazers</h1>
+    <h3>vs</h3>
+    <h1>Pelicans</h1>
+    <h4>Moda Center - Friday, Feb 21</h4>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Lorem ipsum dolor sit amet. consectetur adipiscing
+      elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+      aliquip ex ea commodo consequat.
+    </p>
+    <button id="PrimaryButton">Primary</button>
+    <button id="SecondaryButton">Secondary</button>
+  </HeroImage>,
+  document.getElementById("root")
+);
