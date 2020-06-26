512cfee8ae39355da321f0bcb70cd738983c1f30
Alex Skreen <skreenalex@gmail.com>
2020-06-26 12:57:19 -0700

    add styling. Can't get font to work

--- src/js/PreGame/Hero/index.js
+++ src/js/PreGame/Hero/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import './index.scss';
 import Hero from './Hero';
+import '../../../scss/components/_hero.scss';
+
+
 
 const index = () => {
   return (
     <Hero
-      imageSrc='https://codetheweb.blog/assets/img/posts/full-image-hero/image.jpg'
+      imageSrc='https://www.denverpost.com/wp-content/uploads/2019/04/NUGGETS-TRAILBLAZERS-AAO-_3AO4075x1.jpg'
       color='#555555'
       gradientDirection='to bottom right'
       height='441px'
-      opacity='0.8'
+      opacity='0.2'
       childrenStyles={{ color: '#f7f7f7' }}
       parallax
-      textPosition='center'>
-      <h1>Blazers</h1>
+      textPosition='left'>
+      <h1 className="teams">Blazers</h1>
       <h3>vs</h3>
-      <h1>Pelicans</h1>
+      <h1 className="teams">Pelicans</h1>
       <h4>Moda Center - Friday, Feb 21</h4>
-      <p>
+      <p className="gameDescription">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim adas minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Lorem ipsum dolor sit amet. consectetur adipiscing
-        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-        ut aliquip ex ea commodo consequat.
+        commodo consequat. Lorem ipsum dolor sit amet.
       </p>
-      <button id='PrimaryButton'>Primary</button>
-      <button id='SecondaryButton'>Secondary</button>
+      <button className='primaryButton'>Primary</button>
+      <button className='secondaryButton'>Secondary</button>
     </Hero>
   );
 };
