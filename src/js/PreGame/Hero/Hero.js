5b0cb7baf2abb535f43fd5b874af901b1dbc152d
Alex Skreen <skreenalex@gmail.com>
2020-06-30 09:21:22 -0700

    messing with styling

--- src/js/PreGame/Hero/Hero.js
+++ src/js/PreGame/Hero/Hero.js
@@ -1,5 +1,6 @@
 import React, { Component, Fragment } from 'react';
 import styled from 'styled-components';
+import '../../../scss/components/_hero.scss';
 
 // import generateBgGradient from './utils/generateBgGradient';
 import generateBgGradient from '../utils/generateBgGradient';
--- src/js/PreGame/Hero/index.js
+++ src/js/PreGame/Hero/index.js
@@ -1,33 +1,33 @@
 import React from 'react';
 import './index.scss';
 import Hero from './Hero';
-import '../../../scss/components/_hero.scss';
-
-
 
 const index = () => {
   return (
     <Hero
-      imageSrc='https://www.denverpost.com/wp-content/uploads/2019/04/NUGGETS-TRAILBLAZERS-AAO-_3AO4075x1.jpg'
+      imageSrc='https://codetheweb.blog/assets/img/posts/full-image-hero/image.jpg'
       color='#555555'
       gradientDirection='to bottom right'
       height='441px'
-      opacity='0.2'
+      opacity='0.8'
       childrenStyles={{ color: '#f7f7f7' }}
       parallax
-      textPosition='left'>
-      <h1 className="teams">Blazers</h1>
+      textPosition='center'>
+      <h1>Blazers</h1>
       <h3>vs</h3>
-      <h1 className="teams">Pelicans</h1>
+      <h1>Pelicans</h1>
       <h4>Moda Center - Friday, Feb 21</h4>
-      <p className="gameDescription">
+      <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim adas minim
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
         veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Lorem ipsum dolor sit amet.
+        commodo consequat. Lorem ipsum dolor sit amet. consectetur adipiscing
+        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
+        ut aliquip ex ea commodo consequat.
       </p>
-      <button className='primaryButton'>Primary</button>
-      <button className='secondaryButton'>Secondary</button>
+      <button id='PrimaryButton'>Primary</button>
+      <button id='SecondaryButton'>Secondary</button>
     </Hero>
   );
 };
