ff080742c348b312b516f0ac56f2d70619419fe6
Dusty McCord <Dustymccord@gmail.com>
2020-06-30 09:23:57 -0700

    add basic components to Arena Info and wire up style sheet with basic hieght and width styling.

--- src/js/PreGame/ArenaInfo/index.js
+++ src/js/PreGame/ArenaInfo/index.js
@@ -1,11 +1,18 @@
-import React from 'react';
-
+import React, { Fragment } from 'react';
+import HeaderBarTemplate from '../../Shared/HeaderBarTemplate';
+import '../../../scss/components/_arena-info.scss';
 ///////  ARENA INFO INDEX //////
 const Index = () => {
   return (
-    <div>
-      <h1>ArenaInfo</h1>
-    </div>
+    <Fragment>
+      <HeaderBarTemplate title={'Arena Info'} />
+      <div className='arena'>
+        <div className='arena-logo'>Logo Title</div>
+        <div className='arena-img'>Image</div>
+        <div className='arena-map'>Map</div>
+        <div className='arena-blurb'>Blurb</div>
+      </div>
+    </Fragment>
   );
 };
 
