44882bd15daa7c7bc2634c627174159fb53c65e2
Dusty McCord <Dustymccord@gmail.com>
2020-06-30 10:49:43 -0700

    add arena images and place holder blurb at bottom.

--- src/js/PreGame/ArenaInfo/index.js
+++ src/js/PreGame/ArenaInfo/index.js
@@ -1,5 +1,8 @@
 import React, { Fragment } from 'react';
 import HeaderBarTemplate from '../../Shared/HeaderBarTemplate';
+import rosegardenModaCenter from '../../../media/rosegarden-moda-center.png';
+import modaCenterExt from '../../../media/moda-center-ext.png';
+
 import '../../../scss/components/_arena-info.scss';
 ///////  ARENA INFO INDEX //////
 const Index = () => {
@@ -7,10 +10,29 @@ const Index = () => {
     <Fragment>
       <HeaderBarTemplate title={'Arena Info'} />
       <div className='arena'>
-        <div className='arena-logo'>Logo Title</div>
-        <div className='arena-img'>Image</div>
-        <div className='arena-map'>Map</div>
-        <div className='arena-blurb'>Blurb</div>
+        <div className='arena-logo'>
+          <img src={rosegardenModaCenter} alt='Rose Garden Moda Center' />
+        </div>
+        <div className='arena-img'>
+          <img src={modaCenterExt} alt='Moda Center Exterior' />
+        </div>
+        <div className='arena-map'></div>
+        <div className='arena-blurb'>
+          <div className='arena-blurb-headline'>
+            HOOD'S REHAB CONTINUES ON SCHEDULE DESPITE CORONAVIRUS
+          </div>
+          <div className='arena-blurb-content'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet.
+            consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+            exercitation ullamco laboris nisi ut aliquip ex ea commodo
+            consequat.
+          </div>
+          <button className='arena-blurb-button'>Learn More </button>
+        </div>
       </div>
     </Fragment>
   );
