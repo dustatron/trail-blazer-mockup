69333693931d891353e940fa6f9037c05eec9bb0
Alex Skreen <skreenalex@gmail.com>
2020-06-30 17:27:33 -0700

    scrapped what I had before and re-created. It looks closer to the mock up, but the responsiveness is off

--- src/js/PreGame/Hero/index.js
+++ src/js/PreGame/Hero/index.js
@@ -2,33 +2,40 @@ import React from 'react';
 import './index.scss';
 import Hero from './Hero';
 
+//insert wrapper that contains both columns, then start building out divs with rows. Create two different background colors so you can tell how the gradient is working. 
+
 const index = () => {
   return (
-    <Hero
-      imageSrc='https://codetheweb.blog/assets/img/posts/full-image-hero/image.jpg'
-      color='#555555'
-      gradientDirection='to bottom right'
-      height='441px'
-      opacity='0.8'
-      childrenStyles={{ color: '#f7f7f7' }}
-      parallax
-      textPosition='center'>
-      <h1>Blazers</h1>
-      <h3>vs</h3>
-      <h1>Pelicans</h1>
-      <h4>Moda Center - Friday, Feb 21</h4>
+<main className="flex-container">
+
+    <div className="main-content red">
+      <div className="teamLogos">
+      <img className="blazersLogo" id="blazersLogo" src="https://publish.nba.com/blazers/sites/blazers/files/blazers.png"/>
+      <img className="awayLogo" id="awayLogo" src="https://publish.nba.com/blazers/sites/blazers/files/pelicans.png"/>
+      </div>
+      <div className="teamNames">
+        <h1 className="blazersName">Blazers</h1>
+        <h2>VS</h2>
+        <h1 className="awayName">Pelicans</h1>
+      </div>
+      <div className="gameDescription">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin tincidunt turpis at ligula dictum, eu lobortis justo rutrum. Integer a ex magna. Aenean vel tristique dui, eget aliquet elit. Nulla eleifend tincidunt mattis. Morbi eget ante in sapien lobortis maximus a a nisi. In hac habitasse platea dictumst. Nulla eu.</p>
+      </div>
+      <div className="buttons">
+        <button className="primaryButton" type="button">Primary</button>
+        <button className="secondaryButton" type="button">Secondary</button>
+      </div>
+    </div>
+
+    <aside class="sidebar black">
       <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Lorem ipsum dolor sit amet. consectetur adipiscing
-        elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi
-        ut aliquip ex ea commodo consequat.
+        {/* Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti temporibus recusandae nisi, ipsa esse tempore aperiam laborum libero ipsum at? Accusamus nostrum, atque amet, fugiat voluptate iusto maiores quam similique dolorum vel, voluptatum rerum dignissimos accusantium modi perferendis quidem in! Eum velit aut voluptate ea rerum. Mollitia possimus aut itaque! */}
       </p>
-      <button className='primaryButton'>Primary</button>
-      <button className='secondaryButton'>Secondary</button>
-    </Hero>
+
+    </aside>
+
+  </main>
+
   );
 };
 
