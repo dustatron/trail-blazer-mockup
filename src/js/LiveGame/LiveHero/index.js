a9694e7a91da30d7f774bc53fdd397d411fe6995
Dusty McCord <Dustymccord@gmail.com>
2020-07-02 10:56:39 -0700

    add mock up for header.

--- src/js/LiveGame/LiveHero/index.js
+++ src/js/LiveGame/LiveHero/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import '../../../scss/components/_live-hero.scss';
+import LiveHeroPlaceholderLarge from '../../../media/live-hero-placeholder-large.png';
 ///// Hero \\\\\\
 const Index = () => {
   return (
     <div className='live-hero'>
-      <h1>Hero</h1>
+      <div className='live-hero-top-bar'>
+        <div className='live-hero-top-bar-at'>
+          <span className='live-hero-top-bar-at-live'>Live </span> @ Moda Center
+        </div>
+        <div className='live-hero-top-bar-spacer'>|</div>
+        <div className='live-hero-top-bar-time'>Fri April 28, 2020</div>
+      </div>
+      <div className='live-hero-img'>
+        <img src={LiveHeroPlaceholderLarge} alt='placeholder for video' />
+      </div>
     </div>
   );
 };
