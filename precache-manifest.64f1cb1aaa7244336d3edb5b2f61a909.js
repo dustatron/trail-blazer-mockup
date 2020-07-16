8af293ad33e7e7709b922008bcccbadf1093cea0
Dusty McCord <Dustymccord@gmail.com>
2020-07-16 13:30:11 -0700

    Updates

---
+++ precache-manifest.64f1cb1aaa7244336d3edb5b2f61a909.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d84f2273a8588eedade89458e7ea54d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4f28b794dbe838697b41",
+    "url": "./static/css/main.f57a0153.chunk.css"
+  },
+  {
+    "revision": "e96b4b597bd56adf37fb",
+    "url": "./static/js/2.aca81aa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.aca81aa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f28b794dbe838697b41",
+    "url": "./static/js/main.56dea754.chunk.js"
+  },
+  {
+    "revision": "753c040f1e2268258946",
+    "url": "./static/js/runtime-main.e40ca2e6.js"
+  },
+  {
+    "revision": "d925ce7f52f7dd5668b03165fd6074e5",
+    "url": "./static/media/moda-center-ext.d925ce7f.png"
+  },
+  {
+    "revision": "e8a7da0139de145c3f3dac34980ff395",
+    "url": "./static/media/rosegarden-moda-center.e8a7da01.png"
+  },
+  {
+    "revision": "41a6361ba1b1df462a4cc50a66daef7f",
+    "url": "./static/media/unitedsanscond-heavy.41a6361b.otf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0852392331b2b8062dbfa233ad812501.js"
+  "./precache-manifest.64f1cb1aaa7244336d3edb5b2f61a909.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
